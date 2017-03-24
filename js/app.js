b8e42949beaa06089702faa68c62a720845539fe
Terry Urich <urichterry@gmail.com>
2017-03-24 15:51:54 -0700

    added alert functionality

--- js/app.js
+++ js/app.js
@@ -14,18 +14,37 @@ $(document).ready(function() {
     var score=0;
     $("input[type=radio]:checked").each(function(){
         score+= parseInt($(this).attr("value"));
+        if (score < 20) {
+          alert("We suggest you look into the Ruby on Rails or CSS/Design tracks!");
+        } else if (score > 300) {
+          alert("We suggest you look into the Csharp and DotNet tracks!");
+        } else {
+          alert("We suggest you check out the Android track!");
+        }
     });
 
-    if (score < 20) {
-      $(".survey").hide();
-      $("#ruby").show();
-    } else if (score > 300) {
-      $(".survey").hide();
-      $("#csharp").show();
-    } else {
-      $(".survey").hide();
-      $("#android").show();
-    }
+
+
+// "Hide survey questions and show results message as a paragraph"
+
+
+  // $("#results").click(function(){
+  //   var score=0;
+  //   $("input[type=radio]:checked").each(function(){
+  //       score+= parseInt($(this).attr("value"));
+  //       if (score < 20) {
+  //         $(".survey").hide();
+  //         $("#ruby").show();
+  //       } else if (score > 300) {
+  //         $(".survey").hide();
+  //         $("#csharp").show();
+  //       } else {
+  //         $(".survey").hide();
+  //         $("#android").show();
+  //       }
+  //   });
+
+
 
 
       event.preventDefault();
