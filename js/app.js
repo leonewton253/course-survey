8b170f841adcbbd16b9b35a1488ff19474024cab
Terry Urich <urichterry@gmail.com>
2017-03-30 15:29:04 -0700

    Removed alert

--- js/app.js
+++ js/app.js
@@ -13,45 +13,20 @@ $(document).ready(function() {
   });
 
 
-// "alerts user with suggested track based on score"
 
-  $("#results").click(function(){
+
+  $("#results").click(function(event) {
     var score=0;
     $("input[type=radio]:checked").each(function(){
         score+= parseInt($(this).attr("value"));
         if (score < 10) {
-          alert("We suggest you look into the Ruby on Rails or CSS/Design tracks!");
+          $("#ruby").show();
         } else if (score > 300) {
-          alert("We suggest you look into the Csharp and DotNet tracks!");
+          $("#csharp").show();
         } else {
-          alert("We suggest you check out the Android track!");
+          $("#android").show();
         }
     });
-
-
-
-// "Hide survey questions and show results message as a paragraph instead of alert"
-
-
-  // $("#results").click(function(){
-  //   var score=0;
-  //   $("input[type=radio]:checked").each(function(){
-  //       score+= parseInt($(this).attr("value"));
-  //       if (score < 20) {
-  //         $(".survey").hide();
-  //         $("#ruby").show();
-  //       } else if (score > 300) {
-  //         $(".survey").hide();
-  //         $("#csharp").show();
-  //       } else {
-  //         $(".survey").hide();
-  //         $("#android").show();
-  //       }
-  //   });
-
-
-
-
       event.preventDefault();
     });
 
